82626ae47cf814ea63904b39b500b039a5b9181a
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-02-04 00:27:28 +0100

    Added search bar functionality for searching posts, styling input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { RootStore } from './Store';
@@ -7,23 +7,34 @@ import { GetPost } from './reducers/actions/PostActions';
 function App() {
   const dispatch = useDispatch();
   const postState = useSelector((state: RootStore ) => state.post)
+
+  const [query, setQuery] = useState("")
  
   useEffect(() => {
     dispatch(GetPost());
-  },[]) 
-
+  },[])
+  
   return (
     <div className="App">
-      <div className='card__container'>
-        {postState.post instanceof Array && postState.post.map(posts => (
-            <div className='card'>
-              <div className='card__title'>
-                <img src={`https://picsum.photos/200/200?random=${Math.floor(Math.random() * 40) + 1}`} />
-                <h1>{posts.title}</h1>
-                <p>{posts.body}</p>
+      <input className="search" placeholder="Search Posts" onChange={event => setQuery(event.target.value)} />
+      <div className="card__container">
+        {
+          postState.post instanceof Array && postState.post.filter(post => {
+            if (query === '') {
+              return post;
+            } else if (post.title.toLowerCase().includes(query.toLowerCase()) || post.body.toLowerCase().includes(query.toLowerCase())) {
+              return post;
+            }
+          }).map((post) => (
+            <div className="card">
+              <div className="card__title">
+                <img src={`https://picsum.photos/200/200?random=${Math.floor(Math.random() * 20) + 1}`} />
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
               </div>
-            </div>  
-        ))}
+            </div>
+          ))
+        }
       </div>
     </div>
   );
